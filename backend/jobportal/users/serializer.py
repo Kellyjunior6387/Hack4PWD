c93cc56bc40e836c45d92769a779ff9f149b1d1b
Zamzamke <zamzamchelimo@gmail.com>
2024-10-15 19:14:38 +0300

    user serializer for profile view

--- backend/jobportal/users/serializer.py
+++ backend/jobportal/users/serializer.py
@@ -1,6 +1,6 @@
-# users/serializers.py
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import CustomUser
 
 class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -18,7 +18,7 @@ class UserSerializer(serializers.ModelSerializer):
             last_name=validated_data.get('last_name', '')
         )
         return user
-
+    
 #serializer for user registration
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
@@ -34,11 +34,16 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
         return user
     
-    # Custom serializer for JWT login
+# Custom serializer for JWT login
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
-        # Add custom claims
         token['username'] = user.username
         return token
+    
+# User serializer for profile view
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'bio')
