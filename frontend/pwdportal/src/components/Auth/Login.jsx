ada44bd47a862a7e95b27d21bf767f5a5fda005d
Annsrutto <annsrutto567@gmail.com>
2024-10-15 17:24:54 +0300

    created user login authentication

---
+++ frontend/pwdportal/src/components/Auth/Login.jsx
@@ -0,0 +1,25 @@
+import React, { useState} from 'react'
+
+const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState ("")
+    const [role, setRole] = useState("");
+
+    const [isAuthorized, setIsAuthorized] = useContext(Context);
+
+    const handleLogin = async (e)=> {
+        e.preventDefault();
+        try {
+            const { data } = awa
+        }
+
+    }
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
