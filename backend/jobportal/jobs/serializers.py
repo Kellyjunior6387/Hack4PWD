45a9afe9ddb226104a0a3bca90f3fd62b1259719
Nyams <kellyjunior6386@gmail.com>
2024-10-15 10:32:04 +0300

    Adding the serializer to create a job

--- backend/jobportal/jobs/serializers.py
+++ backend/jobportal/jobs/serializers.py
@@ -1,6 +1,6 @@
 # jobs/serializers.py
 from rest_framework import serializers
-from .models import Job
+from .models import Job, AccessibilityTag
 
 class JobSerializer(serializers.ModelSerializer):
     employer_name = serializers.CharField(source='employer.name')# Fetch employer's name
@@ -14,3 +14,21 @@ class JobDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = ['title', 'employer_name', 'status', 'description_id', 'type']
+
+class JobCreateSerializer(serializers.ModelSerializer):
+    accessibility_tags = serializers.ListField(
+        child=serializers.ChoiceField(choices=AccessibilityTag.TAG_CHOICES),
+        write_only=True
+    )
+    
+    class Meta:
+        model = Job
+        fields = ['title', 'description_id', 'type', 'status', 'accessibility_tags']
+
+    def create(self, validated_data):
+        accessibility_tags = validated_data.pop('accessibility_tags')
+        job = Job.objects.create(**validated_data)
+        # Create accessibility tags associated with the job
+        for tag in accessibility_tags:
+            AccessibilityTag.objects.create(job=job, tag=tag)
+        return job
