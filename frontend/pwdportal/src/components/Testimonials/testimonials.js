ded08607945a0e35d34951f39d81ec0ff6b2009d
Annsrutto <annsrutto567@gmail.com>
2024-10-18 01:10:27 +0300

    created the homepage

---
+++ frontend/pwdportal/src/components/Testimonials/testimonials.js
@@ -0,0 +1,39 @@
+const testimonials = [
+    {
+        name: "David Ochieng",
+        position: "Graphic Designer",
+        company: "CreativeWorks",
+        image: "https://dpemoji.com/wp-content/uploads/2023/01/Whatsapp-dp-for-boys-57.png",
+        text: "AjiraBora gave me the confidence to apply for jobs I thought were out of reach. The accessibility features made the whole process smooth and empowering."
+    },
+    {
+        name: "Mary Wambui",
+        position: "Customer Support Specialist",
+        company: "HelpDesk Inc.",
+        image: "https://t3.ftcdn.net/jpg/06/36/69/86/360_F_636698674_DroChEj5eWmZiaZOSDMnj8hcDqqw74Fp.jpg",
+        text: "As a person with visual impairment, finding an accessible job platform was crucial for me. AjiraBora made it easy to navigate job listings and connect with employers."
+    },
+    {
+        name: "Peter Mwangi",
+        position: "Web Developer",
+        company: "TechBridge",
+        image: "https://dpemoji.com/wp-content/uploads/2023/01/Whatsapp-dp-for-boys-57.png",
+        text: "AjiraBora understood the unique challenges I face as a wheelchair user. Their platform is not only inclusive but also connects me to meaningful opportunities."
+    },
+    {
+        name: "Amina Hassan",
+        position: "Administrative Assistant",
+        company: "OfficePro Solutions",
+        image: "https://t3.ftcdn.net/jpg/06/36/69/86/360_F_636698674_DroChEj5eWmZiaZOSDMnj8hcDqqw74Fp.jpg",
+        text: "AjiraBora's tailored job suggestions and disability-friendly features have transformed how I search for jobs. It’s a platform made for everyone, with everyone in mind."
+    },
+    {
+        name: "Samuel Kiprotich",
+        position: "Digital Marketer",
+        company: "AdVision Media",
+        image: "https://dpemoji.com/wp-content/uploads/2023/01/Whatsapp-dp-for-boys-57.png",
+        text: "AjiraBora is a game-changer for job seekers with disabilities. The platform made my job hunt easier and connected me with employers who value inclusivity."
+    },
+];
+
+export default testimonials;
---
+++ frontend/pwdportal/src/pages/About.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { MetaData } from '../components/MetaData'
+
+export const About = () => {
+  return (
+    <>
+      <MetaData title="About" />
+      <div className="bg-gray-900 min-h-screen pt-14 md:px-20 px-3 text-white">
+  <div className="grid md:grid-cols-3 gap-5 md:px-0 px-2 md:pt-8 pt-4 pb-20">
+    <div className="bg-gray-800 p-5 rounded-lg shadow-lg">
+      <p className="text-4xl pb-3 text-center text-yellow-500">About Us</p>
+      <p className="text-md">
+      At AjiraBora, we are dedicated to empowering persons with disabilities by
+              connecting them with meaningful employment opportunities.<br></br><br/>Our mission is
+              to create an inclusive job portal that bridges the gap between talented
+              individuals with disabilities and employers committed to accessibility.
+              Whether you are seeking your first job or looking to advance your career,
+              JobLink is here to support you every step of the way.
+      </p>
+    </div>
+    <div className="bg-gray-800 p-5 rounded-lg shadow-lg">
+      <p className="text-3xl text-yellow-500 text-center">What Sets Us Apart</p>
+      <ul className="list-disc px-5 text-md">
+        <li>
+          <span className="font-semibold text-lg ">
+          Tailored Accessibility:
+          </span>{" "}
+          We embrace individuality. Our platform is designed to meet the diverse
+          needs of users, whether you navigate using screen readers, require mobility 
+          assistance, or depend on visual or auditory aids. We prioritize inclusivity, 
+          ensuring a seamless experience for everyone.
+        </li>
+        <li>
+          <span className="font-semibold text-lg ">
+            Exceptional Support:
+          </span>{" "}
+          We go beyond just customer service. Our team is committed to your growth, 
+          offering hands-on assistance, from building an optimized profile to providing 
+          expert guidance for job interviews. Your success drives everything we do.
+        </li>
+      </ul>
+    </div>
+    <div className="bg-gray-800 p-5 rounded-lg shadow-lg">
+      <p className="text-2xl text-yellow-500">Join the JobLane Community</p>
+      <p className="pt-3">
+        {" "}When you register with AjiraBora, you're not merely joining a job portal – 
+        you are stepping into a vibrant community dedicated to empowering individuals 
+        with disabilities. Here, you'll connect with like-minded professionals, 
+        supportive recruiters, and experienced mentors who are committed to 
+        fostering a more inclusive workforce.
+      </p>
+      <p className="pt-4">
+        Thank you for choosing AjiraBora as your ally in navigating the job market. 
+        Together, we will unlock a multitude of opportunities and strive for 
+        excellence in every step of your career journey!
+      </p>
+    </div>
+  </div>
+</div>
+
+    </>
+  )
+}
