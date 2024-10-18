818e98bc502e6783889c92c98e925580e0e6a170
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-10-18 15:38:08 +0300

    Changing the db to the render postgress

--- backend/jobportal/jobportal/settings.py
+++ backend/jobportal/jobportal/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -84,6 +85,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES['default'] = dj_database_url.parse('postgresql://nyams:eIn9Ask641QYZKRTjnM8u5nbQ7FUGjG5@dpg-cs958m3qf0us738jinp0-a/ajirabora')
+
 
 
 # Password validation
